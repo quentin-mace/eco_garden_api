a87d2434ac763902c8c88a7058ea7d9e24ff2671
quentin-mace <quentin.mace.1110@gmail.com>
2025-10-16 12:15:23 +0200

    fix validation bug

--- src/Service/JsonValidatorService.php
+++ src/Service/JsonValidatorService.php
@@ -4,17 +4,27 @@ namespace App\Service;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Exception\ExceptionInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
 
 class JsonValidatorService
 {
+    public function __construct(
+        private SerializerInterface $serializer,
+    ){
+    }
+
+    /**
+     * @throws ExceptionInterface
+     */
     public function serializeErrorMessages(ConstraintViolationListInterface $errors): JsonResponse
     {
         $messages = [];
         foreach ($errors as $error) {
             $messages[] = $error->getMessage();
         }
-        $jsonError = $serializer->serialize($messages, 'json');
+        $jsonError = $this->serializer->serialize($messages, 'json');
 
         return new JsonResponse($jsonError, Response::HTTP_BAD_REQUEST, [], true);
     }
