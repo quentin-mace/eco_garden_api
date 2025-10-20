a43c0ceb882fe0f57dd2de8b6b0a9f6069b4d232
quentin-mace <quentin.mace.1110@gmail.com>
2025-10-20 17:35:18 +0200

    Finishing cache

--- src/Controller/MeteoController.php
+++ src/Controller/MeteoController.php
@@ -6,6 +6,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Cache\ItemInterface;
+use Symfony\Contracts\Cache\TagAwareCacheInterface;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Contracts\HttpClient\ResponseInterface;
@@ -21,29 +23,21 @@ final class MeteoController extends AbstractController
      */
     #[Route('/{zipCode}', name: 'app_meteo', methods: ['GET'])]
     #[Route('', name: 'app_meteo', methods: ['GET'])]
-    public function getLocalMeteo(?string $zipCode, HttpClientInterface $client): JsonResponse
+    public function getLocalMeteo(?string $zipCode, HttpClientInterface $client, TagAwareCacheInterface $cache): JsonResponse
     {
         if (!$zipCode) {
             $zipCode = $this->getUser()->getZipCode();
         }
 
-        $coordinates = $this->getCoordinatesFromZipCode($zipCode, $client);
+        $cacheId = 'meteo_' . $zipCode;
 
-        $url = 'https://api.openweathermap.org/data/2.5/weather';
+        $meteo = $cache->get($cacheId, function (ItemInterface $item) use ($zipCode, $client) {
+            $item->tag('meteo');
+            $coordinates = $this->getCoordinatesFromZipCode($zipCode, $client);
+            return $this->getMeteoFromCoordinates($coordinates, $client);
+        });
 
-        $response = $client->request(
-            'GET',
-            $url,
-            [
-                'query' => [
-                    'lat' => $coordinates['lat'],
-                    'lon' => $coordinates['lon'],
-                    'appid' => self::API_KEY,
-                ]
-            ]
-        );
-
-        return new JsonResponse($response->getContent(), $response->getStatusCode(), [], true);
+        return new JsonResponse($meteo['content'], $meteo['status'], [], true);
     }
 
     private function getCoordinatesFromZipCode(string $zipCode, HttpClientInterface $client): array
@@ -65,6 +59,28 @@ final class MeteoController extends AbstractController
         return [
             'lat' => $data['lat'],
             'lon' => $data['lon'],
+        ];
+    }
+
+    private function getMeteoFromCoordinates(array $coordinates, HttpClientInterface $client): array
+    {
+        $url = 'https://api.openweathermap.org/data/2.5/weather';
+
+        $meteo = $client->request(
+            'GET',
+            $url,
+            [
+                'query' => [
+                    'lat' => $coordinates['lat'],
+                    'lon' => $coordinates['lon'],
+                    'appid' => self::API_KEY,
+                ]
+            ]
+        );
+
+        return [
+            'content' => $meteo->getContent(),
+            'status' => $meteo->getStatusCode(),
         ];
     }
 }
